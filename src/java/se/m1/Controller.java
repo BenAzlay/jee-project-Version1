a41edc358e14e5a254a8f74696e6773df1e667d5
BenAzlay <34835201+BenAzlay@users.noreply.github.com>
2020-01-10 16:08:44 +0100

    Update Controller.java

--- src/java/se/m1/Controller.java
+++ src/java/se/m1/Controller.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.m1;
 
 import java.io.IOException;
@@ -19,10 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import static se.m1.Constants.*;
 
-/**
- *
- * @author JAA
- */
 public class Controller extends HttpServlet {
 
     DBActions dba;
@@ -34,16 +25,6 @@ public class Controller extends HttpServlet {
     String userLogin = ""; 
     String userPwd = "";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param session
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     * @throws java.sql.SQLException
-     */
     protected void processRequest(HttpServletRequest session, HttpServletResponse response)
             throws ServletException, IOException, SQLException {
         Properties prop = new Properties();
@@ -144,15 +125,6 @@ public class Controller extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -163,14 +135,6 @@ public class Controller extends HttpServlet {
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -181,11 +145,6 @@ public class Controller extends HttpServlet {
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
